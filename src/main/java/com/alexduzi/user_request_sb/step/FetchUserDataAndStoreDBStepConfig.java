426225dd4acd564b147551868cdfc79e15c7412e
alexduzi <duzihd@gmail.com>
2025-06-02 19:10:55 -0300

    adding step

---
+++ src/main/java/com/alexduzi/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -0,0 +1,33 @@
+package com.alexduzi.user_request_sb.step;
+
+import com.alexduzi.user_request_sb.dto.UserDTO;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class FetchUserDataAndStoreDBStepConfig {
+
+    private final PlatformTransactionManager transactionManager;
+
+    @Value("${chunkSize}")
+    private int chunkSize;
+
+    public FetchUserDataAndStoreDBStepConfig(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader, JobRepository jobRepository) {
+        return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
+                .<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+                .reader(fetchUserDataReader)
+                .build();
+    }
+}
