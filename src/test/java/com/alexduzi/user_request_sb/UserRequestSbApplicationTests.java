0aeb9937ac42cf2cd599c736c894a0efdb8bda0f
alexduzi <duzihd@gmail.com>
2025-06-02 18:47:05 -0300

    first commit

---
+++ src/test/java/com/alexduzi/user_request_sb/UserRequestSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexduzi.user_request_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
